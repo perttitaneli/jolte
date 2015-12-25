0f1ebe6020bfbaa7396413ca512474007652a4b8
Jaana Kulmala <jaana.j.kulmala@gmail.com>
2015-12-25 20:34:21 +0200

    Automated commit at 2015-12-25 18:34:21 UTC by middleman-deploy 1.0.0

--- config.rb
+++ config.rb
@@ -28,6 +28,33 @@
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  blog.permalink = "{title}.html"
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "sivut/{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  blog.layout = "person"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  #blog.tag_template = "tag.html"
+  #blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
+end
+
 ###
 # Helpers
 ###
@@ -36,9 +63,9 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+    activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -77,3 +104,5 @@ activate :deploy do |deploy|
   deploy.method = :git
   deploy.build_before = true
 end
+
+activate :directory_indexes
