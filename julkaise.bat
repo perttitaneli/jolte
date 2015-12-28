a66f15a292e47c83dc77052d9c5bb915b8f53daf
kkulmala <kari.kulmala@gmail.com>
2015-12-28 14:20:26 +0200

    julkaise.bat

--- julkaise.bat
+++ julkaise.bat
@@ -2,11 +2,11 @@ git pull
 git add .
 git commit -m "julkaise.bat"
 
-REM IF EXISTS "%JOLTE_USERNAME%" IF EXISTS "%JOLTE_PASSWORD%" GOTO SET
+IF NOT "%JOLTE_USERNAME%" == "" IF NOT "%JOLTE_PASSWORD%" == "" GOTO SET
 
-REM :NOT_SET
-REM git push
-REM GOTO END
+:NOT_SET
+git push
+GOTO END
 
 :SET
 git push --repo https://%JOLTE_USERNAME%:%JOLTE_PASSWORD%@github.com/perttitaneli/jolte.git
